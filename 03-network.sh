28bb2c045a06fdca4ffba396090501b49c72c0de
Akshat Pradhan <no@example.com>
2023-12-25 23:35:05 -0600

    change network settings

--- 03-network.sh
+++ 03-network.sh
@@ -3,7 +3,7 @@
 echo "Firewall"
 ## Options
 # [x] Enable Firewall (default: off)
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on &>/dev/null
+#sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on &>/dev/null
 defaults write /Library/Preferences/com.apple.alf globalstate 1 >/dev/null 2>&1
 
 # [ ] Block all incoming connection (default: off)
@@ -12,7 +12,7 @@ defaults write /Library/Preferences/com.apple.alf globalstate 1 >/dev/null 2>&1
 # [x] Automatically allow built-in software to receive incoming connections (default: on)
 # [x] Automatically allow downloaded signed software to receive incoming connections (default: on)
 # [x] Enable stealth mode (default: off)
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on &>/dev/null
+#sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on &>/dev/null
 defaults write /Library/Preferences/com.apple.alf stealthenabled 1 >/dev/null 2>&1
 echo "end Firewall"
 
